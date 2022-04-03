eb2e27a3c328b69fbcae3cc2be397c89a0b34857
ValeevTimur <timik.ru@gmail.com>
2022-04-03 17:12:26 +0300

    DZ_LESSON_3

---
+++ valeev_timur_dz_4/task_4_4.py
@@ -0,0 +1,15 @@
+from utils import currency_rates
+print(currency_rates("USD"))
+print(currency_rates("noname"))
+print(currency_rates("BGN"))
+print(currency_rates("JPY"))
+
+"""
+C:\Users\Печенька\AppData\Local\Programs\Python\Python310\python.exe C:/Users/Печенька/PycharmProjects/TIVAL2/valeev_timur_dz_4/task_4_4.py
+83.4285
+None
+47.1294
+67.8418
+
+Process finished with exit code 0
+"""
